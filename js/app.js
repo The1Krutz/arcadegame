e2bddb357d156c2808df7f901cf1590395592443
Katrinka Swenson <kjswenso@gmail.com>
2018-06-14 23:17:12 -0500

    Six enemy bugs scroll across screen

--- js/app.js
+++ js/app.js
@@ -1,16 +1,8 @@
-/* Create classes and subclasses 
-*  Characters
-*  Enemies extends Characters
-*  Player extends Characters
-*/
-
 class Characters {
     constructor(x, y) {
         this.x = x;
         this.y = y;
     };
-
-    update(dt) {};
 
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -19,13 +11,21 @@ class Characters {
 }
 
 class Enemy extends Characters {
-    constructor(x = 50, y = 63, speedX = 0, speedY = 0) {
+    constructor(x = 50, y = 63, speed) {
         super(x, y);
         this.sprite = 'images/enemy-bug.png';
-        this.speedX = speedX;
-        this.speedY = speedY;
+        this.speed = speed;
         allEnemies.push(this);
     }
+
+    update(dt) {
+        if(this.x < 506) {
+            this.x += this.speed * dt;
+        }
+        else {
+            this.x = -300;
+        }
+    };
 }
 
 class Player extends Characters {
@@ -34,39 +34,67 @@ class Player extends Characters {
         this.sprite = 'images/char-horn-girl.png';
     };
 
+    //distance player moves upon keypress
+
     handleInput(allowedKeys) {
         switch(allowedKeys) {
             case "left": 
                 this.x -= 100;
                 break;
             case "up":
-                this.y -= 84;
+                this.y -= 86;
                 break;
             case "right": 
                 this.x += 100;
                 break; 
             case "down": 
-                this.y += 84;
+                this.y += 86;
                 break;
         }
     }
 
+//boundaries for the player object to not move off board
+//player enters other side of screen classic arcade style
     update() {
         if (this.x < 5) {
-        this.x = 405;
+            this.x = 405;
         }
         else if (this.x > 405) {
             this.x = 5;
         }
 
        if (this.y < 0) {
-           // reset();
-           this.y = 400;
+           this.y = 0;
         }
         else if (this.y > 400) {
             this.y = 400;
         }
+
+        winGame();
     }
+}
+
+
+const resetPlayer = () => {
+  player.x = 205;
+  player.y = 400;
+}
+
+const win = () => {
+    alert("You've won! Great job dodging those bugs!");
+}
+
+function winGame() {
+    setTimeout(function(){
+        if (player.y === 0) {
+            win();
+            resetPlayer();
+        }
+    }, 200);
+}
+
+const checkWin = () => {
+  (player.y === -10) ? winner() : false;
 }
 
 
@@ -86,30 +114,25 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };*/
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 
 const allEnemies = [];
-const enemy1 = new Enemy(50, 63);
-const enemy2 = new Enemy(50, 147);
-const enemy3 = new Enemy(50, 230);
+
+const enemy1 = new Enemy(-10, 63, 100);
+const enemy2 = new Enemy(-300, 63, 100);
+const enemy3 = new Enemy(-10, 147, 50);
+const enemy4 = new Enemy(-300, 147, 50);
+const enemy5 = new Enemy(-10, 231, 25);
+const enemy6 = new Enemy(-300, 231, 25)
 
 
 // Place the player object in a variable called player
 const player = new Player(205, 400); 
-
 
 
 // This listens for key presses and sends the keys to your
